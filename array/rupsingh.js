36ad89b7cfca14f5d1645af404d0e44daa165b8a
Rupsingh Chouhan <Rupsingh Chouhan>
2016-12-19 21:10:29 +0530

    Array file added

---
+++ array/rupsingh.js
@@ -0,0 +1,8 @@
+//Homogeneous
+var homogeneous = ["Lenovo", "HP", "Sony"];
+
+//heterogeneous
+var heterogeneous = ["Lenovo", 999, 5+5];
+
+//multidimensions
+var multidimensions = ["Lenovo",[0, "HP", [5+5, 500]], "Sony"];
