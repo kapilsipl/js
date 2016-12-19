48d92290ed3a8dee12f64094739d80d42f9630c6
RupsingC <rupsingh.chouhan@systematixindia.com>
2016-12-19 21:24:24 +0530

    Array file updated

--- array/rupsingh.js
+++ array/rupsingh.js
@@ -1,8 +1,13 @@
 //Homogeneous
 var homogeneous = ["Lenovo", "HP", "Sony"];
-
+homogeneous[2];
+sony
 //heterogeneous
 var heterogeneous = ["Lenovo", 999, 5+5];
+heterogeneous[1];
+999
 
 //multidimensions
 var multidimensions = ["Lenovo",[0, "HP", [5+5, 500]], "Sony"];
+multidimensions[0][2][0];
+10
