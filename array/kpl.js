e46b34e71822e9dacd01067d98b01ac8c59f7583
Kapil <kapil.chhabra@systematixindia.com>
2016-12-19 20:11:42 +0530

    New changes done

---
+++ array/kpl.js
@@ -0,0 +1,17 @@
+/*Array sample*/
+/*Normal Homogeneous*/
+var userName = ["User A", "User B", "User C"];
+demoArry[0]
+
+/*Normal Heterogeneous*/
+var demoArry = ["Section A", 9999, "Section C"];
+demoArry[1]
+
+/*Normal Multidimensional*/
+var multiDemo = ["Demo a", ["Demo 2", 9999, 5+5], "Demo 3"]
+multiDemo[1][2];
+multiDemo[3];
+
+/*Normal Multidimensional*/
+var multiDemoThrice = ["Demo a", ["Demo 2", 9999, [5+5, "Girish", ["Third row", 3]]], "Demo 3"]
+multiDemoThrice[1][2][2][0];
