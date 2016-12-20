9c802ed7574e50e3594ccd043c33fbf12d39edaf
Kapil <kapil.chhabra@systematixindia.com>
2016-12-20 20:36:41 +0530

    New changes added in Object file

--- object/kpl.js
+++ object/kpl.js
@@ -9,3 +9,24 @@ var objCar = {
 
 	}
 }
+
+/*Object creation*/
+var newObj = {demoA: "Value A"}
+console.log(newObj.demoA);
+
+/*New val add in Obj*/
+
+newObj.newVal = "New value";
+console.log(newObj.demoA);
+
+var newObj = {demoA: "Value A", child:{
+	childA: "Child A",
+	numbersB: 80,
+	numbersC: 10,
+	numbersD: 20
+}}
+newObj.demoB = " Chhabra";
+newObj.demoA += " Kapil";
+newObj.child.numbersB -= 10;
+newObj.child.numbersC *= 10;
+newObj.child.numbersD %= 7;
