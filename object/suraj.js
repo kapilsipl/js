9d31b98fcfe9fb63cc8a5707f9c930942bc89236
surajgautam123 <suraj.gautam@systematixindia.com>
2016-12-20 21:06:55 +0530

    Add files via upload

---
+++ object/suraj.js
@@ -0,0 +1,16 @@
+var colorobj = {
+	red: "10",
+	blue: "20",
+	green: "30",
+	lightColor:{
+		lightRed: "0.4",
+		lightBlue: "0.4",
+		lightGreen: "0.4",
+	},
+	otherColor:{
+		white: "1",
+		black: "0",
+	}
+}
+
+console.log(colorobj.red, colorobj.lightColor.lightGreen);
