c8906955fab44e456df1c45902ba569e3a615191
Kapil Chhabra <kapil.chhabra@systematixindia.com>
2016-12-22 00:24:35 +0530

    availability.js
    calculation of present

---
+++ avaliablity.js
@@ -0,0 +1,10 @@
+function avaliablity(a) {return a*100/9;}
+avaliablity(9);
+avaliablity(8);
+avaliablity(7);
+avaliablity(6);
+avaliablity(5);
+avaliablity(4);
+avaliablity(3);
+avaliablity(2);
+avaliablity(1);
